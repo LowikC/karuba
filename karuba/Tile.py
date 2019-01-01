25201c7eefe364318e338499bc3b7ad5c69fe96c
Lowik Chanussot <lowik@fb.com>
2019-01-01 14:09:21 +0100

    Add all tiles

--- karuba/Tile.py
+++ karuba/Tile.py
@@ -22,13 +22,7 @@ class Tile:
     ways: Ways
 
 
-# Temp
 tiles = {
-    i: Tile(uid=1, has_cristal=False, has_gold=False, ways=Ways(left=True, right=True))
-    for i in range(1, 37)
-}
-
-tilesb = {
     1: Tile(uid=1, has_cristal=False, has_gold=False, ways=Ways(left=True, right=True)),
     2: Tile(uid=2, has_cristal=False, has_gold=False, ways=Ways(left=True, right=True)),
     3: Tile(
@@ -90,5 +84,73 @@ tilesb = {
     ),
     18: Tile(
         uid=18, has_cristal=False, has_gold=False, ways=Ways(top=True, bottom=True)
+    ),
+    19: Tile(
+        uid=19, has_cristal=False, has_gold=False, ways=Ways(right=True, left=True)
+    ),
+    20: Tile(
+        uid=20, has_cristal=False, has_gold=False, ways=Ways(right=True, left=True)
+    ),
+    21: Tile(
+        uid=21, has_cristal=False, has_gold=False, ways=Ways(right=True, bottom=True)
+    ),
+    22: Tile(
+        uid=22, has_cristal=False, has_gold=False, ways=Ways(left=True, bottom=True)
+    ),
+    23: Tile(
+        uid=23,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(right=True, top=True, bottom=True),
+    ),
+    24: Tile(
+        uid=24,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(left=True, top=True, bottom=True),
+    ),
+    25: Tile(
+        uid=25,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(left=True, top=True, bottom=True, right=True),
+    ),
+    26: Tile(
+        uid=27,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(left=True, top=True, bottom=True, right=True),
+    ),
+    27: Tile(uid=27, has_cristal=True, has_gold=False, ways=Ways(top=True, right=True)),
+    28: Tile(uid=28, has_cristal=False, has_gold=False, ways=Ways(top=True, left=True)),
+    29: Tile(
+        uid=29,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(top=True, bottom=True, right=True),
+    ),
+    30: Tile(
+        uid=30,
+        has_cristal=False,
+        has_gold=False,
+        ways=Ways(top=True, bottom=True, left=True),
+    ),
+    31: Tile(
+        uid=31, has_cristal=False, has_gold=False, ways=Ways(top=True, bottom=True)
+    ),
+    32: Tile(
+        uid=32, has_cristal=False, has_gold=True, ways=Ways(top=True, bottom=True)
+    ),
+    33: Tile(
+        uid=33, has_cristal=True, has_gold=False, ways=Ways(top=True, bottom=True)
+    ),
+    34: Tile(
+        uid=34, has_cristal=False, has_gold=True, ways=Ways(top=True, bottom=True)
+    ),
+    35: Tile(
+        uid=35, has_cristal=False, has_gold=False, ways=Ways(top=True, bottom=True)
+    ),
+    36: Tile(
+        uid=36, has_cristal=False, has_gold=False, ways=Ways(top=True, bottom=True)
     ),
 }
